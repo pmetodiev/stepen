bd93e7ef286676cf13e38b0bd0d1729f3633bb8b
pmetodiev <plame478@gmail.com>
2022-03-13 11:56:18 +0200

    stepen

---
+++ src/stepen/stepen.java
@@ -0,0 +1,14 @@
+package stepen;
+
+public class stepen {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(-20,2));
+        System.out.println(Math.pow(92,2));
+        System.out.println(Math.pow(638,2));
+        System.out.println(Math.pow(72,2));
+        System.out.println(Math.pow(200,2));
+        System.out.println(Math.pow(-22,2));
+
+
+    }
+}
